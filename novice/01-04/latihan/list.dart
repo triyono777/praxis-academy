fdcc560426ac8a897691744dda46a422137426ca
triyono <yono.tri@gmail.com>
2019-11-07 09:44:08 +0700

    growable list

--- novice/01-04/latihan/list.dart
+++ novice/01-04/latihan/list.dart
@@ -1,10 +1,21 @@
 main() {
-// Fixed Length List
+  /// Fixed Length List
+  /// pada deklarasi list jumlah anggota dari list di tuliskan
   var daftarAngka = List(5);
   daftarAngka[0] = 1;
   daftarAngka[1] = 5;
   daftarAngka[2] = 7;
   daftarAngka[3] = 3;
   daftarAngka[4] = 22;
-  print(daftarAngka);
+  // print(daftarAngka);
+
+  /// Growable List
+  /// pada deklarasi List tidak di cantumkan jumlah listnya
+
+  var daftarYangLain = List();
+  daftarYangLain.add(8);
+  daftarYangLain.add(3);
+  daftarYangLain.add(6);
+  daftarYangLain.add(88);
+  print(daftarYangLain);
 }
