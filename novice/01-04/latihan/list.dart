ef3cd8259572d320a1c885cea1ebcbe238c5e2ef
triyono <yono.tri@gmail.com>
2019-11-07 09:54:57 +0700

    element List

--- novice/01-04/latihan/list.dart
+++ novice/01-04/latihan/list.dart
@@ -17,5 +17,21 @@ main() {
   daftarYangLain.add(3);
   daftarYangLain.add(6);
   daftarYangLain.add(88);
-  print(daftarYangLain);
+  // print(daftarYangLain);
+
+  ///    element list
+
+  var listPertama = List();
+  listPertama.add(8);
+  listPertama.add(3);
+  listPertama.add(5);
+  listPertama.add(11);
+
+  // print('ini = ${listPertama.first} adalah list yang pertama'); //list yg pertama
+  // print(listPertama.isEmpty); // berupa boolean , tru or false
+  // print(listPertama.isNotEmpty); // berupa boolean , tru or false
+  // print(listPertama.length); // panjang atau jumlah anggota list
+  // print(listPertama.last); // list terakhir
+  // print(listPertama.reversed); // mengubah urutan list terbalik
+  // print(listPertama.single);  // hanya bisa digunakan apabila anggota list hanya 1.
 }
