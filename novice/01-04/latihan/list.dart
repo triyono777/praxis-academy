bc62788d1f26201f65179c173cf1d16d8b4d2df2
triyono <yono.tri@gmail.com>
2019-11-07 09:39:37 +0700

    fixed list

---
+++ novice/01-04/latihan/list.dart
@@ -0,0 +1,10 @@
+main() {
+// Fixed Length List
+  var daftarAngka = List(5);
+  daftarAngka[0] = 1;
+  daftarAngka[1] = 5;
+  daftarAngka[2] = 7;
+  daftarAngka[3] = 3;
+  daftarAngka[4] = 22;
+  print(daftarAngka);
+}
