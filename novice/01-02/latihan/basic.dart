892c5ff0d27431d9562ffdcdca04a669629ec431
triyono <yono.tri@gmail.com>
2019-11-11 10:27:23 +0700

    penambahan kasus pada 02-01

--- novice/01-02/latihan/basic.dart
+++ novice/01-02/latihan/basic.dart
@@ -1,8 +1,10 @@
+// mendefinisikan sebuang fungsi
 printInteger(int aNumber) {
-  print('nomorny adalah $aNumber');
+  print('nomorny adalah $aNumber'); // print ke console
 }
 
+//setiap aplikasi dart harus menggun
 main() {
-  var nomor = 55;
-  printInteger(nomor);
+  var nomor = 55; // deklarasi dan inisiasi variabel
+  printInteger(nomor); // memanggil fungsi
 }
