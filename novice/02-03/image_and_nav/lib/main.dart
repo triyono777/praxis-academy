8bf3fefa385006ad6db421c225ed0d9a9c25b447
triyono <yono.tri@gmail.com>
2019-11-13 16:14:45 +0700

    latigan route

--- novice/02-03/image_and_nav/lib/main.dart
+++ novice/02-03/image_and_nav/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'latihan_routing.dart';
 
 void main() => runApp(HeroApp());
 
 class HeroApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(_) {
     return MaterialApp(
       title: 'Transition Demo',
       home: MainScreen(),
+      routes: <String, WidgetBuilder>{
+        '/pertama': (BuildContext context) => FirstRoute(),
+        '/kedua': (BuildContext context) => SecondRoute(),
+      },
     );
   }
 }
@@ -19,26 +24,64 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('ONE PIECE CREW'),
       ),
-      body: GestureDetector(
-        child: Center(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                Hero(tag: 'imageHero', child: Image.asset('images/luffy.jpeg')),
-                Hero(tag: 'imageHero1', child: Image.asset('images/zoro.jpg')),
-                Hero(tag: 'imageHero2', child: Image.asset('images/nami.jpg')),
-                Hero(
-                    tag: 'imageHero3', child: Image.asset('images/choper.jpg')),
-              ],
-            ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text(
+                'Crew Member SHP',
+                style: TextStyle(fontSize: 30.0),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+              GestureDetector(
+                child: Center(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: <Widget>[
+                        Hero(
+                            tag: 'imageHero',
+                            child: Image.asset('images/luffy.jpeg')),
+                        Hero(
+                            tag: 'imageHero1',
+                            child: Image.asset('images/zoro.jpg')),
+                        Hero(
+                            tag: 'imageHero2',
+                            child: Image.asset('images/nami.jpg')),
+                        Hero(
+                            tag: 'imageHero3',
+                            child: Image.asset('images/choper.jpg')),
+                      ],
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      // MaterialPageRoute(builder: (BuildContext context)
+                      MaterialPageRoute(builder: (_) {
+                    return DetailScreen();
+                  }));
+                },
+              ),
+              MyButton(
+                label: 'button satu',
+                iconnya: Icon(Icons.web),
+                routing: '/pertama',
+              ),
+              MyButton(
+                label: 'button dua',
+                iconnya: Icon(Icons.whatshot),
+                routing: '/kedua',
+              ),
+              MyButton(
+                label: 'button tiga',
+                iconnya: Icon(Icons.work),
+              ),
+            ],
           ),
-        ),
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return DetailScreen();
-          }));
-        },
+        ],
       ),
     );
   }
@@ -56,6 +99,41 @@ class DetailScreen extends StatelessWidget {
         onTap: () {
           Navigator.pop(context);
         },
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  var routing;
+  Icon iconnya;
+  var label;
+
+  MyButton({this.routing, this.iconnya, this.label});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(routing);
+          print('sudah dipencet');
+        },
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: iconnya,
+              iconSize: 50.0,
+              onPressed: () {
+                Navigator.of(context).pushNamed(routing);
+              },
+            ),
+            Text(
+              label,
+              style: TextStyle(fontSize: 20.0),
+            )
+          ],
+        ),
       ),
     );
   }
