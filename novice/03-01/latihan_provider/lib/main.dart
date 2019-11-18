35cdb783c861d9751aa070a3cc9060b9333b2c37
triyono <yono.tri@gmail.com>
2019-11-18 12:10:41 +0700

    penambahan change notifier dan consumer

--- novice/03-01/latihan_provider/lib/main.dart
+++ novice/03-01/latihan_provider/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_provider/aplication_color.dart';
 import 'package:provider/provider.dart';
 
 main() => runApp(MyApp());
@@ -7,47 +8,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Provider',
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              AnimatedContainer(
-                margin: EdgeInsets.all(5),
-                width: 100,
-                height: 100,
-                color: Colors.lightBlue,
-                duration: Duration(microseconds: 500),
+      home: ChangeNotifierProvider<AplicationColor>(
+        builder: (context) => AplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Consumer<AplicationColor>(
+              builder: (context, aplicationcolor, _) => Text(
+                'Provider',
+                style: TextStyle(color: aplicationcolor.warna),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Consumer<AplicationColor>(
+                  builder: (context, aplicationcolor, _) => AnimatedContainer(
+                    margin: EdgeInsets.all(5),
+                    width: 100,
+                    height: 100,
+                    color: aplicationcolor.warna,
+                    duration: Duration(microseconds: 500),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.all(5),
+                        child: Text(
+                          'Red accent',
+                          style: TextStyle(color: Colors.redAccent),
+                        )),
+                    Consumer<AplicationColor>(
+                      builder: (context, aplicationcolor, _) => Switch(
+                        value: aplicationcolor.isLightBlue,
+                        onChanged: (valueBaru) {
+                          aplicationcolor.isLightBlue = valueBaru;
+                        },
+                      ),
+                    ),
+                    Container(
                       margin: EdgeInsets.all(5),
                       child: Text(
-                        'Red accent',
-                        style: TextStyle(color: Colors.redAccent),
-                      )),
-                  Switch(
-                    value: true,
-                    onChanged: (valueBaru) {},
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(5),
-                    child: Text(
-                      'light blue',
-                      style: TextStyle(color: Colors.lightBlue),
-                    ),
-                  )
-                ],
-              ),
-            ],
+                        'light blue',
+                        style: TextStyle(color: Colors.lightBlue),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
