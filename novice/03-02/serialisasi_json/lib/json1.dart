77d92097c6f40153b18cf7d1bef9393769a99ae3
triyono <yono.tri@gmail.com>
2019-11-19 10:00:51 +0700

    latihan serialisasi json

---
+++ novice/03-02/serialisasi_json/lib/json1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JsonSatu extends StatefulWidget {
+  @override
+  _JsonSatuState createState() => _JsonSatuState();
+}
+
+class _JsonSatuState extends State<JsonSatu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ novice/03-02/serialisasi_json/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        // '/menu1': (BuildContext context) => SilverAppBarContoh(),
+        // '/menu2': (BuildContext context) => SilverAppBarContoh2(),
+        // '/menu3': (BuildContext context) => SliverGridContoh(),
+        // '/menu4': (BuildContext context) => LatihanSliver(),
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Serialisasi JSON'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            MyButton(
+              iconnya: Icon(Icons.account_balance),
+              judul: 'Json 1',
+            ),
+            MyButton(
+              iconnya: Icon(Icons.pages),
+              judul: 'Json 2',
+            ),
+            MyButton(
+              iconnya: Icon(Icons.offline_bolt),
+              judul: 'Json 3',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  final iconnya;
+  final routing;
+  final judul;
+  MyButton({this.iconnya, this.routing, this.judul});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        FlatButton(
+          child: Row(
+            children: <Widget>[
+              iconnya,
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+              ),
+              Text(
+                judul,
+                style: TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+          onPressed: () {
+            Navigator.of(context).pushNamed(routing);
+          },
+        )
+      ],
+    );
+  }
+}
