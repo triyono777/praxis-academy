a02ca5a379ba943ed85f545422622faffa80d762
triyono <yono.tri@gmail.com>
2019-11-20 15:41:13 +0700

    add kasus chat firebase

---
+++ novice/03-03/kasus/flutter_chat/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: Center(
+        child: Text('halo chat'),
+      ),
+    );
+  }
+}
