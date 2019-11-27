4e8e1c0875abe605416c5c8c8e909a8608822e5b
triyono <yono.tri@gmail.com>
2019-11-27 13:05:24 +0700

    latihan url launcher

--- novice/04-02/halopaket/CHANGELOG.md
+++ novice/04-02/halopaket/CHANGELOG.md
@@ -3,6 +3,12 @@
   
 ## [0.0.2] - 26 Nov 2019
 * penambahan info device
+  
+## [0.0.3] - 26 Nov 2019
+* perubahan nama
+
+## [0.0.4] - 26 Nov 2019
+* perubahan nama awal
 
 
 
---
+++ novice/04-03/url_launcher_latihan/lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'URL Launcher',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'URL Launcher'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _launched;
+
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchInWebViewOrVC(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: true, forceWebView: true);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return Text('');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String toLaunch = 'https://flutter.dev';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(toLaunch),
+            ),
+            RaisedButton(
+              onPressed: () => setState(() {
+                _launched = _launchInBrowser(toLaunch);
+              }),
+              child: Text('Launch in browser'),
+            ),
+            Padding(padding: EdgeInsets.all(16.0)),
+            RaisedButton(
+              onPressed: () => setState(() {
+                _launched = _launchInWebViewOrVC(toLaunch);
+              }),
+              child: Text('Launch in app'),
+            ),
+            Padding(padding: EdgeInsets.all(16.0)),
+            FutureBuilder<void>(future: _launched, builder: _launchStatus),
+          ],
+        ),
+      ),
+    );
+  }
+}
